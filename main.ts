8f40b39fb2f04ea6da12dca6b9bada3027f09c77
TsjOgawa <44085195+TsjOgawa@users.noreply.github.com>
2021-03-08 19:53:59 +0900

    ble2

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
 bluetooth.onBluetoothConnected(function () {
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
     basic.showIcon(IconNames.Yes)
 })
 bluetooth.onBluetoothDisconnected(function () {
